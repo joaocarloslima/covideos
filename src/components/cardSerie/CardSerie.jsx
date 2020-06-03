ee32fcdf378495d47b5b8b801dc30c76e16f18a2
João Carlos <joao@etecia.com.br>
2020-06-03 11:41:12 -0300

    componentes

---
+++ src/components/cardSerie/CardSerie.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './CardSerie.css'
+
+export default function CadSerie() {
+  return (
+    <div className="card-serie">
+      <span className="material-icons fav-btn">add_circle_outline</span>
+      <div className="poster-image"></div>
+      <div className="card-detail">
+        <span className="material-icons">star</span>
+          9,5
+          <h1>Lost</h1>
+        <h2>01/01/2020</h2>
+      </div>
+      <a href="#" className="btn-detail">
+        <span className="material-icons">play_circle_filled</span>
+        detalhes
+      </a>
+    </div>
+  );
+}
