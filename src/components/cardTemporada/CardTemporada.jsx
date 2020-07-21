61c502011aeb57932cbe12008d8c53c66feb2273
João Carlos <joao@etecia.com.br>
2020-07-21 12:34:35 -0300

    :beetle: poster padrão card temporada

--- src/components/cardTemporada/CardTemporada.jsx
+++ src/components/cardTemporada/CardTemporada.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import './CardTemporada.css'
+import deafultImgUrl from "./default_poster.png"
+
 
 export default function CardTemporada({ temporada }){
     return(
         <div class="card-temporada">
             <img 
                 src={`https://image.tmdb.org/t/p/w200/${temporada.poster_path}`} 
+                onError={(e)=>{e.target.onerror = null; e.target.src=deafultImgUrl}}
                 alt="poster temporada"
             />
             <div class="temporada-info">
